bf4b4002817fd536746a9d13a915eeb4595e167a
Zhe Xie <zhexie@usc.edu>
2014-09-08 11:22:32 -0700

    hash map itself

--- First Missing Positive.cpp
+++ First Missing Positive.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    // point: do it without using extra space.
-    // put A[i] onto its desired position. 
-    // for example, if A[0] == 4, then 4 should be put onto A[3], then we swap A[0] and A[3], then A[3] == 4. 
-    int firstMissingPositive(int A[], int n) {
+    // use itself as a hashmap. For example, if A[2] = 6, then we flip A[6-1] to negative, 
+    // the first missing positive will be the first non-flip number
+    int firstMissingPositive(int A[], int n){ 
+        // eliminate negative numbers
         for(int i = 0; i < n; i++){
-            while(A[i] != i+1){
-                if(A[i] <= 0 || A[i] > n) break; // not within the range
-                
-                int targetIndex = A[i] - 1;
-                if(A[targetIndex] == A[i]) break; // already occupied, duplicates
-                // swap
-                A[i] = A[targetIndex];
-                A[targetIndex] = targetIndex+1;
+            A[i] = A[i] <= 0 ? n+1 : A[i]; // negative numbers can't be candidates
+        }
+        
+        for(int i = 0; i < n; i++){
+            int targetIndex = abs(A[i]) - 1;
+            if(0 <= targetIndex && targetIndex < n && A[targetIndex] > 0){
+                A[targetIndex] *= -1; // flip
             }
         }
-        // find the first missing number
+        
         for(int i = 0; i < n; i++){
-            if(A[i] != i+1)
+            if(A[i] >= 0)
                 return i+1;
         }
         return n+1;
