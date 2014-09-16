4be5be94a7ef00176a76b4b16e32734a87ba1ea1
Zhe Xie <zhexie@usc.edu>
2014-09-16 14:48:20 -0700

    revised Valid Number

--- _Valid Number.cpp
+++ _Valid Number.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
-    // hard
+    // hard: consider corner cases
     // 1. regular expression 2. Finate Autumata Machine: http://blog.csdn.net/kenden23/article/details/18696083
     // [+/-] d.d (E/e)[+/-]d
     bool isNumber(const char *s) {
         int i = 0;
+        int j = strlen(s)-1;
         
         // remove spaces
         while(i < strlen(s) && s[i] == ' ') i++;
-        if(i == strlen(s)) return false;
-        int j = strlen(s)-1;
         while(j >= 0 && s[j] == ' ') j--;
-        if(j < 0) return false;
+        if(i == strlen(s) || j < 0) return false;
         
         if(s[i] == '+' || s[i] == '-') i++;
         
         bool num = false;
         bool dot = false;
-        bool exp = false;
+        bool exp = false; // exponential
+        
         while(i <= j){
             if(s[i] >= '0' && s[i] <= '9') 
                 num = true;
             else if(s[i] == '.'){
+                if(dot) return false; // can't repeat
                 if(exp) return false;
-                if(dot) return false;
                 dot = true;
             }
             else if(s[i] == 'e'){
-                if(exp) return false;
+                if(exp) return false; // can't repeat
                 if(!num) return false;
                 exp = true;
                 num = false;
@@ -39,6 +39,6 @@ public:
                 return false;
             i++;
         }
-        return num;
+        return num; // end with number
     }
 };
