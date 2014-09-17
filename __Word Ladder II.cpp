f64530915ce2252e2e346224f3107c2af6c96159
Zhe Xie <zhexie@usc.edu>
2014-09-16 17:31:01 -0700

    solved star matching!

--- __Word Ladder II.cpp
+++ __Word Ladder II.cpp
@@ -1,45 +1,39 @@
 class Solution {
 public:
-    // dfs: Time Limit Exceeded. 
-    // bfs+backtracking: fucked up
-    vector<vector<string>> findLadders(string start, string end, unordered_set<string> &dict) {
-        vector<vector<string>> ret;
-        vector<string> ladder;
-        ladder.push_back(start);
-        set<string> visited; // avoid duplicate ladders
-        visited.insert(start);
-		int min = INT_MAX;
-        helper(start, end, dict, ret, visited, ladder, min);
-        return ret;
-    }
-    void helper(string start, string end, unordered_set<string> &dict, vector<vector<string>> &ret, set<string> &visited, vector<string> ladder, int &min){
-        if(start == end){
-            if(ladder.size() == min){
-                ladder.push_back(end);
-                ret.push_back(ladder);
+    // two "star" pointers for going back to match
+    bool isMatch(const char *s, const char *p) {
+        const char *star_p = NULL; // store the position of the last "*"
+        const char *star_p_s = s; // store the mapping position of the last "*" in s
+        
+        while(*s){
+            // normal case
+            if(*p == '?' || *p == *s){
+                p++;
+                s++;
+                continue;
             }
-            if(ladder.size() < min){ // update min level, which is time consuming
-                ret.clear();
-                ladder.push_back(end);
-                ret.push_back(ladder);
-				min = ladder.size();
+            
+            // "*" appears, match zero times
+            if(*p == '*'){
+                star_p = p; 
+                star_p_s = s;
+                p++;
+                continue;
             }
-            return;
-        }
-    
-        for(int i = 0; i < start.size(); i++){
-            char ori = start[i];
-            for(char c = 'a'; c <= 'z'; c++){
-                start[i] = c;
-                if(dict.find(start) != dict.end() && visited.find(start) == visited.end()){
-                    visited.insert(start);
-                    ladder.push_back(start);
-                    helper(start, end, dict, ret, visited, ladder, min);
-                    ladder.pop_back();
-                    visited.erase(start);
-                }
+            
+            // can't move on, go back to use "*", to match one/more times
+            if(star_p){
+                p = star_p + 1;
+                star_p_s++;
+                s = star_p_s;
+                continue;
             }
-            start[i] = ori;
-        }  
+            
+            // no matching 
+            return false;
+        } 
+        
+        while(*p == '*') p++;
+        return !*p;
     }
 };
