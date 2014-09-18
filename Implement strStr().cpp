e14970c30c51dde40ff2f2497d06226160154391
Zhe Xie <zhexie@usc.edu>
2014-09-18 15:28:16 -0700

    revised strStr()

--- Implement strStr().cpp
+++ Implement strStr().cpp
@@ -2,17 +2,20 @@ class Solution {
 public:
     // KMP
     char *strStr(char *haystack, char *needle) {
-        if(NULL == haystack || NULL == needle) return NULL;
+        if(haystack == NULL || needle == NULL){
+            return NULL;
+        }
         int hLen = strlen(haystack);
         int nLen = strlen(needle);
         if(hLen < nLen) return NULL;
-        for(int i = 0; i + nLen <= hLen; i++){
+        for(int i = 0; i + nLen - 1 < hLen; i++){
             char *p = &haystack[i];
             int j = 0;
-            for( ; j < nLen; j++){
+            while(j < nLen){
                 if(*p != needle[j])
                     break;
                 p++;
+                j++;
             }
             if(j == nLen)
                 return &haystack[i];
