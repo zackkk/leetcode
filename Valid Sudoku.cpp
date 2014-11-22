ff25e273e4f1a0623e119607b8627c4829bb0cec
Zhe Xie <zhexie@usc.edu>
2014-11-21 20:03:22 -0800

    Valid Sudoku Improved

--- Valid Sudoku.cpp
+++ Valid Sudoku.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
+    // more space less time
+    bool isValidSudoku(vector<vector<char> > &board){
+        vector<vector<bool>> rows(9, vector<bool>(9, false));
+        vector<vector<bool>> cols(9, vector<bool>(9, false));
+        vector<vector<bool>> subs(9, vector<bool>(9, false)); // sub boards
+        
+        for(int i = 0; i < 9; ++i){
+            for(int j = 0; j < 9; ++j){
+                if(board[i][j] == '.') continue;
+                int c = board[i][j] - '1';
+                if(rows[i][c] || cols[j][c] || subs[i/3*3+j/3][c]) return false;
+                rows[i][c] = cols[j][c] = subs[i/3*3+j/3][c] = true;
+            }
+        }
+        return true;
+    }
+    
+    
+    
     // straight-forward implementation
+    /*
     bool isValidSudoku(vector<vector<char> > &board) {
         // check rows
         for(int i = 0; i < 9; i++){
@@ -38,4 +58,5 @@ public:
         
         return true;
     }
+    */
 };
