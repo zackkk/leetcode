12a3fde25e18fa4684df809d4ddeaf1ff66988cc
Zhe Xie <zhexie@usc.edu>
2014-11-30 21:52:58 -0800

    Elegant Solution for Text Justification :)

--- Text Justification.cpp
+++ Text Justification.cpp
@@ -1,59 +1,36 @@
 class Solution {
 public:
-    // multiple cases
-    // point: record words length in each line, then we can assign spaces easily
+    // concise :)
     vector<string> fullJustify(vector<string> &words, int L) {
         vector<string> ret;
-        if(words.size() == 0 || L == 0) { ret.push_back(""); return ret;} // corner case 
+        int n = words.size();
+        if(n == 0 || L == 0) { ret.push_back(""); return ret; }
         
-        for(int i = 0; i < words.size(); ){
-            vector<string> curLineWords;
-            int curLineWordsLen = 0;     
-            // previous length + length needed < L
-            while((i < words.size() && curLineWordsLen == 0) ||   // first word
-                  (i < words.size() && curLineWordsLen + (curLineWords.size() - 1) + (words[i].size() + 1) <= L)) {  // check if can add
-                curLineWords.push_back(words[i]);
-                curLineWordsLen += words[i].size();
-                i++;
+        int i = 0;
+        while(i < n){
+           vector<string> ws;  // words of current line
+           int ws_len = 0;
+           while((i < n && ws.size() == 0) ||
+                 (i < n && ws_len + ws.size() - 1 + words[i].size() + 1 <= L)){
+                     ws.push_back(words[i]);
+                     ws_len += words[i].size();
+                     i++;
+                 }
+                 
+            int space = L - ws_len;
+            if(i == n || ws.size() == 1){ //special cases: last line / one word in a line
+                for(int j = 0; j < ws.size() - 1 && space > 0; ++j, --space) ws[j] += " "; // bug: use "&&" !
+                for( ; space > 0; --space) ws[ws.size()-1] += " ";
             }
-            int space = L - curLineWordsLen;
-            /*
-                case1:     last line with one word / multiple words;
-                case2: non-last line with one word / multiple words;
-             */
+            else{
+                while(space > 0){
+                    for(int j = 0; j < ws.size() - 1 && space > 0; ++j, --space) ws[j] += " ";
+                }
+            }
             
-            // last line, not distribute evenly
-            if(i == words.size()){ 
-                // assign one for previous words
-                if(curLineWords.size() > 1){
-                    for(int j = 0; j < curLineWords.size()-1; j++){ 
-                        if(space <= 0) break; // bug happened here
-                        curLineWords[j] += ' ';
-                        space--;
-                    }
-                }   
-                // assign the rest for the last one
-                while(space > 0){curLineWords[curLineWords.size()-1] += ' '; space--; }
-            }
-            // distribute evenly
-            else{ 
-                if(curLineWords.size() > 1){
-                    while(space > 0){
-                        for(int j = 0; j < curLineWords.size()-1; ++j){ 
-                            if(space <= 0) break; // bug happened here
-                            curLineWords[j] += ' ';
-                            space--;
-                        }
-                    }
-                }
-                else{
-                    while(space > 0){curLineWords[0] += ' '; space--; }
-                }
-            }
-          
-            string tmp;
-            for(int j = 0; j < curLineWords.size(); j++) tmp += curLineWords[j];
-            ret.push_back(tmp);
+            string str;
+            for(string tmp : ws) str += tmp;
+            ret.push_back(str);
         }
         return ret;
     }
